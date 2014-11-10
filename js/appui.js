056fc2e05b11976f3dbb9b14446367ee572ef603
burakkarakaya <burakkarakaya1984@gmail.com>
2014-11-10 16:54:20 +0200

    update

--- js/appui.js
+++ js/appui.js
@@ -1,18 +1,23 @@
+// PLUGINS
+
+/* Generate a random uuid. http://www.broofa.com */
+(function(){var e="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");Math.uuid=function(d,c){var b=[],a;c=c||e.length;if(d)for(a=0;a<d;a++)b[a]=e[0|Math.random()*c];else{var f;b[8]=b[13]=b[18]=b[23]="-";b[14]="4";for(a=0;36>a;a++)b[a]||(f=0|16*Math.random(),b[a]=e[19==a?f&3|8:f])}return b.join("")};Math.uuidFast=function(){for(var d=Array(36),c=0,b,a=0;36>a;a++)8==a||13==a||18==a||23==a?d[a]="-":14==a?d[a]="4":(2>=c&&(c=33554432+16777216*Math.random()|0),b=c&15,c>>=4,d[a]=
+e[19==a?b&3|8:b]);return d.join("")};Math.uuidCompact=function(){return"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g,function(d){var c=16*Math.random()|0;return("x"==d?c:c&3|8).toString(16)})}})();
+
 // Global Variable
-var con = $('#appWizard'), scene = $('.scene', con), background = $('.background', scene), menu = $('ul.settings li a', con); 
+var con = $('#appWizard'), scene = $('.scene', con), background = $('.background', scene), menu = $('ul.settings li a', con), outputArea = $('.output', con), outputObj = {}; 
 
 // Draggable, Resizable
 (function(window){
-	function AppUI( obj, callback ){
+	function AppUI( obj, objID, callback ){
 
 		var template = obj["template"] || '',
 			settings = obj["settings"] || {}
 			resizable = obj["resizable"] || false,
 			type = obj["type"] || 'button',
 			output = '',
-			ID = DefaultValue(); 
-			
-			
+			ID = DefaultValue(),
+			objPos = {}; 
 			
 		if( ID != undefined ){ 
 			
@@ -22,19 +27,26 @@ var con = $('#appWizard'), scene = $('.scene', con), background = $('.background
 			
 			//EVENT
 			ID.draggable({ containment:'parent'	}).on('dragstop', function( event, ui ){
-				if( callback != undefined ) callback();
+				objPos['{{left}}'] = ui.position.left;
+     			objPos['{{top}}'] = ui.position.top;
+				generateOutput();
 			});
 
 			if( resizable ){
 				ID.addClass('resizable').resizable({ containment:'parent' }).on('dragstop', function( event, ui ){
-					console.log('sdsadsd');
+					objPos['{{width}}'] = ui.size.width;
+	     			objPos['{{height}}'] = ui.size.height;
+					generateOutput();
 				});
 			}
 			
-			$('a.removeBtn', ID).bind('click', function(){
+			$('a.removeBtn', ID).unbind('click').bind('click', function(){
 				var con = $( this ).parents('.box');
-				//if( resizable ) con.resizable('destroy')
-				con.draggable('destroy').remove();
+				if( con.hasClass('resizable') ) 
+					con.resizable('destroy');
+					con.draggable('destroy').remove();
+				
+				deleteOutputObj( objID ); //delete obj	
 			});
 			$('a.settingsBtn', ID).bind('click', function(){
 				$( this ).parents('span.settings').toggleClass('opened');
@@ -83,10 +95,21 @@ var con = $('#appWizard'), scene = $('.scene', con), background = $('.background
 			var theme = '<ul class="settingList">';
 				for( var i in settings ){
 					var s = settings[ i ];
-					theme += '<li><span>'+ s['title'] +'</span><input type="text" value="'+ s['defaultValue'] +'" rel="'+ s['slug'] +'" /></li>';
+					theme += '<li><span>'+ s['title'] +'</span><input type="text" class="'+ i +'" value="'+ s['defaultValue'] +'" rel="'+ s['slug'] +'" /></li>';
 				}
 				theme += '</ul>';
 			return theme;	
+		}
+		
+		function generateOutput(){
+			var theme = template;
+			
+			// Position & Size
+			for( var i in objPos ){
+				theme = theme.replace( i, objPos[ i ] );
+			}
+			
+			console.log( theme );
 		}
 		
 		function output(){
@@ -95,7 +118,7 @@ var con = $('#appWizard'), scene = $('.scene', con), background = $('.background
 		
 		//GLOBAL VARIABLE
 		this.getOutput = function(){
-			return 
+			return output;
 		}
 	};
 	window.AppUI = AppUI;
@@ -110,11 +133,29 @@ menu.bind('click', function(){
 });
 
 function createConfig( obj ){
-	new AppUI( obj );
+	var id = Math.uuid( 5 );
+	outputObj[ id ] = new AppUI( obj, id );
 }
+
+function deleteOutputObj( id ){
+	if( outputObj[ id ] != null ){
+		outputObj[ id ] = null;
+		delete outputObj[ id ];
+		generateHtml();
+	}
+}
+
+//GENERATE BUTTON
+$('a.generateBtn').bind('click', generateHtml);
+function generateHtml(){
+	var html = '';
+	for( var i in outputObj )
+		html += outputObj[ i ].getOutput();
+	outputArea.val( html );
+}
+
 
 //FILE UPLOAD
 $('.fileUpload').bind('change', function(){
 	console.log( $( this ).val() );
 });
-
--- js/config.js
+++ js/config.js
@@ -2,21 +2,27 @@
 var config = {
 	
 	'text': {
-		'template': '<div class="content"><h2 class="title">{{title}}</h2><p class="desc">{{desc}}</p></div>',
+		'template': '<div class="content" style="width:{{width}}px;height:{{height}}px;top:{{top}}px;left:{{left}}px;"><h2 class="title">{{title}}</h2><p class="desc">{{desc}}</p></div>',
 		'settings': {
 			'title':{ 'title': 'TITLE', 'slug': '{{title}}', 'defaultValue': 'title' },
-			'desc':{ 'title': 'DESCRIPTION', 'slug': '{{desc}}', 'defaultValue': 'description' }
+			'desc':{ 'title': 'DESCRIPTION', 'slug': '{{desc}}', 'defaultValue': 'description' },
+			'width':{ 'title': 'WIDTH', 'slug': '{{width}}', 'defaultValue': '' },
+			'height':{ 'title': 'HEIGHT', 'slug': '{{height}}', 'defaultValue': '' },
+			'top':{ 'title': 'TOP', 'slug': '{{top}}', 'defaultValue': '' },
+			'left':{ 'title': 'LEFT', 'slug': '{{left}}', 'defaultValue': '' }
 		},
 		'resizable': true,
 		'type': 'textButton'
 	},
 	
 	'link': {
-		'template': '<a href="{{link}}" class="{{class}}"><span>{{buttonText}}</span><i></i></a>',
+		'template': '<a href="{{link}}" class="{{class}}" style="top:{{top}}px;left:{{left}}px;"><span>{{buttonText}}</span><i></i></a>',
 		'settings': {
 			'link':{ 'title': 'TARGET URL', 'slug': '{{link}}', 'defaultValue': '#' },
 			'class':{ 'title': 'BUTTON CLASS', 'slug': '{{class}}', 'defaultValue': '' },
-			'buttonText':{ 'title': 'BUTTON TEXT', 'slug': '{{buttonText}}', 'defaultValue': 'BUTTON' }
+			'buttonText':{ 'title': 'BUTTON TEXT', 'slug': '{{buttonText}}', 'defaultValue': 'BUTTON' },
+			'top':{ 'title': 'TOP', 'slug': '{{top}}', 'defaultValue': '' },
+			'left':{ 'title': 'LEFT', 'slug': '{{left}}', 'defaultValue': '' }
 		},
 		'resizable': false,
 		'type': 'linkButton'
@@ -25,6 +31,5 @@ var config = {
 	'hitArea': {
 	
 	}
-	
 	
 };
